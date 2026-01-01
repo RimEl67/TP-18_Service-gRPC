a79c5ad1d2b1620e60a95dbc661ffa04c5bc7d51
RimEL60 <h.rimoulax@gmail.com>
2026-01-01 16:14:44 +0100

    first commit

---
+++ src/main/java/com/example/tp18/Tp18ServiceGRpcAvecSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.tp18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp18ServiceGRpcAvecSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp18ServiceGRpcAvecSpringBootApplication.class, args);
+    }
+}
